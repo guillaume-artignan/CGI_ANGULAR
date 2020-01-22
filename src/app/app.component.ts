81ffa718987dee48474314a9005cc3d70d257771
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-01-22 12:47:03 +0100

    Developpement Articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'premiere-app';
+  title = 'PREMIERE APP';
 }
