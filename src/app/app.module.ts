4d06aedee959a8cf6361989069553e9e535dd9e7
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-01-23 10:23:19 +0100

    Scientifiques - Two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ScientifiqueComponent } from './scientifique/scientifique.component';
@@ -12,7 +12,7 @@ import { ScientifiqueComponent } from './scientifique/scientifique.component';
     ScientifiqueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [ScientifiqueComponent]
